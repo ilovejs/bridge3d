9685d89df65dcf3fdf7c8526fe59b1bdd0d1107c
ilovejs <zhuangdeyouxiang@gmail.com>
2021-09-08 20:01:52 +1000

    release

--- src/index.js
+++ src/index.js
@@ -47,7 +47,10 @@ function init() {
 
   loader.load(
     // "https://cdn.jsdelivr.net/gh/digitalcoleman/3DModels@main/myBridge1.glb",
-    "http://localhost:1234/myBridge1.glb",
+    // "http://localhost:1234/myBridge1.glb",
+    // "https://github.com/ilovejs/bridge3d/blob/master/src/myBridge2.glb?raw=true",
+    // "https://cdn.jsdelivr.net/gh/ilovejs/bridge3d@1/src/myBridge2.glb",
+    "https://cdn.jsdelivr.net/gh/ilovejs/bridge3d@1.1/src/totem.glb",
     function (gltf) {
       scene.add(gltf.scene);
       render(); //render the scene for the first time
